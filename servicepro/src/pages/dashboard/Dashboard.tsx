b74d1af74959e6881f4eb6903c0dd1275d8159f6
JASSADHAMMI <jassadhammi@gmail.com>
2026-01-29 21:14:56 +0530

    add

--- servicepro/src/pages/dashboard/Dashboard.tsx
+++ servicepro/src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,8 @@
 export default function Dashboard() {
-    return <h2>Dashboard Page</h2>;
+    return (
+        <>
+            <h2>Dashboard Page</h2>
+            <h2>jassa</h2>
+        </>
+    );
 }
