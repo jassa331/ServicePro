63871cc8595a61edf6232bc441e5c927e8be44d7
jasmeet singh <jasmeet.singh@caresmartz360.com>
2026-02-02 15:17:52 +0530

    add

--- servicepro/src/components/layout/MainLayout.tsx
+++ servicepro/src/components/layout/MainLayout.tsx
@@ -1,8 +1,10 @@
-import { Outlet } from "react-router-dom";
+﻿import { Outlet, useNavigate } from "react-router-dom"; // import useNavigate
 import NavMenu from "../layout/NavMenu";
 import "../../assets/css/MainLayout.css";
 
 export default function MainLayout() {
+    const navigate = useNavigate(); // ✅ inside component
+
     return (
         <div className="layout">
             {/* LEFT SIDEBAR */}
@@ -13,6 +15,37 @@ export default function MainLayout() {
                 {/* TOP BAR */}
                 <div className="top-bar">
                     <h3>Dashboard</h3>
+
+                    <input
+                        type="text"
+                        placeholder="Find"
+                        className="search-input"
+                    />
+
+                    <button className="icon-btn">
+                        <i className="fa-solid fa-gear"></i>
+                    </button>
+                    <button className="icon-btn">
+                        <i className="fa-solid fa-calendar-days"></i>
+                    </button>
+                    <button className="icon-btn">
+                        <i className="fa-solid fa-bell"></i>
+                    </button>
+                    <button className="icon-btn">
+                        <i className="fa-solid fa-comment"></i>
+                    </button>
+                    <button
+                        className="icon-btn"
+                        onClick={() => alert("Invoice clicked!")}
+                    >
+                        <i className="fa-file-invoice"></i>
+                    </button>
+                    <button
+                        className="icon-btn"
+                        onClick={() => navigate("/address-booking")}
+                    >
+                        <i className="fa-solid fa-location-dot"></i>
+                    </button>
                 </div>
 
                 {/* PAGE CONTENT */}
